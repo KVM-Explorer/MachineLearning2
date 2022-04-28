306124ea1e8bfbcde91469e800a79f60f0d70ca6
KVM <wfcyywh1225@126.com>
2022-04-28 11:54:04 +0800

    添加和归一化数据和范书求解

--- KNN.py
+++ KNN.py
@@ -2,41 +2,67 @@ import heapq
 import math
 import torch
 
-def euclidean_distance(feature1,feature2):
-    return torch.sqrt(torch.sum(torch.pow((feature1+feature2),2)))
+
+# =====================距离求解======================
+# L2 范数
+def euclidean_distance(feature1, feature2):
+    return torch.sqrt(torch.sum(torch.pow((feature1 + feature2), 2)))
+
+
+# L1 范数
+def manhattan_distance(feature1, feature2):
+    return torch.sum(torch.abs(feature1 - feature2))
+
+
+# L 无穷范数
+def chebyshev_distance(feature1, feature2):
+    # TODO fix
+    return torch.max(torch.abs(feature1 - feature2))
+
+
+# ==================== 核函数=========================
+
+
+
+# ====================预处理函数=======================
+# 归一化数据最大最小值归一化
+def normalize(vector: torch.tensor):
+    return torch.nn.functional.normalize(vector,p=torch.inf,dim=0)
 
 
 class KNN:
-    def __init__(self,k):
+    def __init__(self, k):
         self.kf = None
+        self.df = None
         self.features = None
         self.labels = None
         self.k = k
 
     class Item:
-        def __init__(self,dis,label):
+        def __init__(self, dis, label):
             self.dis = dis;
             self.label = label;
 
-        def __iter__(self,other):
-            if self.dis < other.dis :
+        def __iter__(self, other):
+            if self.dis < other.dis:
                 return True
             else:
                 return False;
 
-    def Train(self,features,labels):
+    def Train(self, features, labels):
         self.features = features
         self.labels = labels
 
-    def Detect(self,feature):
+    def Detect(self, feature):
         dis_list = []
-        for i in range(0,len(self.features)):
-            dis = self.kf(self.features[i],feature) # Todo 计算核函数
-            heapq.heappush(dis_list,self.Item(dis,self.labels[i]))      # 默认小根堆
+        feature_kf = self.kf(feature)
+        for i in range(0, len(self.features)):
+            dis = self.df(self.kf(self.features[i]), feature)  # Todo 计算核函数
+            heapq.heappush(dis_list, self.Item(dis, self.labels[i]))  # 默认小根堆
 
-        label_count = {0:0,1:0,2:0}
+        label_count = {0: 0, 1: 0, 2: 0}
         label = 0
-        for i in range(0,self.k):
+        for i in range(0, self.k):
             tmp = heapq.heappop(dis_list)
             label_count[tmp.label] += 1
             if label_count[tmp.label] > label_count[label]:
@@ -45,6 +71,5 @@ class KNN:
         self.features.add(feature)
         self.labels.add(label)
 
-    def DefineKF(self,kernal_function):
+    def DefineKF(self, kernal_function):
         self.kf = kernal_function;
-
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import KNN
 import torch
+
 if __name__ == '__main__':
     feature1 = torch.tensor([3,4])
     feature2 = torch.tensor([0,0])
     print(KNN.euclidean_distance(feature1,feature2))
+
+    vector = torch.tensor([[3,4,10,10],[10,10,100,100]],dtype=torch.float32) # 注意torch保持原有变量类型
+    print(KNN.normalize(vector))
