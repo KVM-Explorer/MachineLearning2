e316e1f8620323f5423a330f0023ad22e2656ec8
KVM <wfcyywh1225@126.com>
2022-05-04 16:22:57 +0800

    完成交叉验证选取K值代码

---
+++ AdjustingParametersTest.py
@@ -0,0 +1,11 @@
+import torch
+
+from utils import optimization
+
+if __name__ == "__main__":
+    features = torch.load("data/train/vectors.pt")  # 存储batch
+    labels = torch.load("data/train/labels.pt")
+
+    # 交叉验证最佳K值
+    best_k = optimization.k_cross_validation(features=features,labels=labels,block_size=9)
+    print(best_k)
